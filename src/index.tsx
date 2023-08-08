c6610f075810e070f1e219b0d192dbb292fdf959
Alexander1995-it <sany_nax@mail.ru>
2023-08-08 10:42:08 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+
+import { App } from "./App.tsx";
+import reportWebVitals from "./reportWebVitals.ts";
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+root.render(<App />);
+
+reportWebVitals();
